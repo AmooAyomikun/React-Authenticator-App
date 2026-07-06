3125a300a7cdc8b60886e85e8b1d57499e644406
Amoo Quadri <amooquadri555@gmail.com>
2026-07-06 18:50:49 +0100

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import AuthProvider from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <HashRouter base="/<React-Authenticator-App>">
       <AuthProvider>
         <App />
       </AuthProvider>
